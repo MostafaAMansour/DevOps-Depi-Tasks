40a2ce46dc2fa3a78d9dc86549f94c166e628ad5
Mostafa Mansour <85360001+MostafaAMansour@users.noreply.github.com>
2024-08-20 21:23:56 +0300

    updated

---
+++ Task16/jenkins-template/libraries/github/steps/build.groovy
@@ -0,0 +1,10 @@
+def call(Map params) {
+    node() {
+        stage('Checkout') {
+            checkout([$class: 'GitSCM', 
+                branches: [[name: "${params.BRANCH}"]], 
+                userRemoteConfigs: [[url: "${params.GITHUB_REPO}", credentialsId: "${params.GITHUB_CREDENTIALS_ID}"]]
+            ])
+        }
+    }
+}
